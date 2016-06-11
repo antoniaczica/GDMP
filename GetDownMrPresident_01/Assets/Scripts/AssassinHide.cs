4819569907b22a50ec215d40aa2ac9b13bbe700b
Antonia Caskey <antonia.caskey@gmail.com>
2016-06-11 18:14:33 +1200

    Assassin hide 1
    assassin can hide

---
+++ GetDownMrPresident_01/Assets/Scripts/AssassinHide.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssassinHide : MonoBehaviour {
+
+	Vector3 lightSize;
+	void Start () {
+		lightSize = GameObject.Find ("Cube").transform.localScale;
+	}
+
+	void Update () {
+		if (Input.GetKey ("space")) {
+			HideOrShow(new Vector3(0,0,0));
+		} else {
+			HideOrShow(lightSize);
+		}
+	}
+
+	// changes size of object for hiding or showing
+	void HideOrShow(Vector3 size){
+		GameObject.Find ("Cube").transform.localScale = size;
+	}
+}
